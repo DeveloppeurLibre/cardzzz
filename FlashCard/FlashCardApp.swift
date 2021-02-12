38344b3935e2a058540484383f7d74bcd88c34d9
Quentin Cornu <contact@developpeurlibre.com>
2021-02-12 10:37:55 +0100

    Initial Commit

---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Quentin Cornu on 12/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
